54d395fb6b9923f3f5241cee60767df0a914200e
Joel Velasquez <150091426+joelEVR@users.noreply.github.com>
2024-03-31 21:47:46 -0400

    Fixxed Comments And some errors in Javadocs
    Signed-off-by: Joel Velasquez <150091426+joelEVR@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,12 +47,6 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.11.0")
     implementation("com.google.code.gson:gson:2.10.1")
 
-    implementation("androidx.annotation:annotation:1.7.1")
-    implementation(fileTree(mapOf(
-        "dir" to "C:\\Users\\joesv\\AppData\\Local\\Android\\Sdk\\platforms\\android-34",
-        "include" to listOf("*.aar", "*.jar")
-    )))
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
